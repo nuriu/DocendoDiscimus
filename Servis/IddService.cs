4a24b5c29426d2a0e864204e08145506158075dc
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-12-11 21:16:06 +0300

    Menü dinamiği eklendi.

--- Servis/IddService.cs
+++ Servis/IddService.cs
@@ -1,11 +1,5 @@
 ﻿using Servis.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
 
 namespace Servis
 {
---
+++ istemci/HtmlHelpers/HtmlHelperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace İstemci.HtmlHelpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string ActivePage(this HtmlHelper helper, string action, string controller)
+        {
+            string sinifDegeri = "";
+
+            string currentController = helper.ViewContext.Controller.ValueProvider.GetValue("controller").RawValue.ToString();
+            string currentAction = helper.ViewContext.Controller.ValueProvider.GetValue("action").RawValue.ToString();
+
+            if (currentController == controller && currentAction == action)
+            {
+                sinifDegeri = "active";
+            }
+
+            return sinifDegeri;
+        }
+    }
+}
